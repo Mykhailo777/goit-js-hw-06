612e61d248911ef5f46f7b778afd14a5abd4829e
Mykhailo Khilchuk <mishahil2000@gmail.com>
2022-05-10 22:16:30 +0300

    працює

---
+++ js/task-06.js
@@ -0,0 +1,35 @@
+//Напиши скрипт, який під час втрати фокусу на інпуті(подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+//Яка кількість смиволів повинна бути в інпуті, зазначається в його атрибуті data-length.
+//Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+
+// #validation-input {
+//   border: 3px solid #bdbdbd;
+// }
+
+// #validation-input.valid {
+//   border-color: #4caf50;
+// }
+
+// #validation-input.invalid {
+//   border-color: #f44336;
+// }
+
+
+let inputVal = document.getElementById("validation-input");
+
+let totalLenght = inputVal.getAttribute("data-length");
+let intTotallenght = parseInt(totalLenght, 10);
+
+inputVal.oninput = function() {
+  if (inputVal.value.length === intTotallenght) {
+    inputVal.classList.remove("invalid");
+    inputVal.classList.add("valid");
+  }
+  if (inputVal.value.length === 0) {
+    inputVal.classList.remove("valid");
+    inputVal.classList.remove("invalid");
+  }
+  if (inputVal.value.length !== intTotallenght && inputVal.value.length !== 0) {
+    inputVal.classList.add("invalid");
+  }
+};
