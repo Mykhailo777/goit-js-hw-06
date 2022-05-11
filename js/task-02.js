684416adf7f7b0be8866b51e8ef697e444119fa5
Mykhailo Khilchuk <mishahil2000@gmail.com>
2022-05-11 15:21:35 +0300

    виводить спикок інгрідієнтів за 1 раз

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+let ulList = document.getElementById("ingredients");
+const foodIngredients = ingredients.forEach(ingredient => {
+  let item = document.createElement("li");
+  item.innerHTML = ingredient;
+  ulList.append(item);
+});
+
+console.log(foodIngredients);
