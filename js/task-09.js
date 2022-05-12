0f4acfc097c7ff5ce9232269e156fbc271f680db
Mykhailo Khilchuk <mishahil2000@gmail.com>
2022-05-12 18:56:57 +0300

    при кліку фон змінює колір

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
+const bodyColor = document.querySelector("body");
+const buttonColor = document.querySelector(".change-color")
+const colorName = document.querySelector(".color")
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButtonColor = () => {
+  bodyColor.style.backgroundColor=getRandomHexColor();
+  colorName.textContent=bodyColor.style.backgroundColor
+}
+
+buttonColor.addEventListener("click",changeButtonColor)
+
